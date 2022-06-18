d02302685bf5f29bb67efc2d0e5f090e8704d813
Markus-Rost <37275477+Markus-Rost@users.noreply.github.com>
2022-06-18 23:35:32 +0200

    make package

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const {properties: {wikiProjects: {items: {properties: wikiProjectSchema}}}} = require('./projects-schema.json');
+
 /**
  * A MediaWiki project
  * @typedef {object} WikiProject
@@ -17,42 +19,63 @@
  * @property {?string} note - Note about the specific project
  */
 
-const {wikiProjects: jsonWikiProjects} = require('./projects.json');
+/**
+ * @type {{
+ *     inputToWikiProject: Map<string, ?{fullArticlePath: string, fullScriptPath: string, wikiProject: WikiProject}>,
+ *     urlToIdString: Map<string, ?string>,
+ *     idStringToUrl: Map<string, ?string>
+ * }}
+ */
+const functionCache = {
+	inputToWikiProject: new Map(),
+	urlToIdString: new Map(),
+	idStringToUrl: new Map()
+};
 
 /**
  * List of MediaWiki projects
  * @type {WikiProject[]}
  */
-const wikiProjects = jsonWikiProjects.map( wikiProject => {
+const wikiProjects = require('./projects.json').wikiProjects.map( wikiProject => {
 	if ( wikiProject.idString ) {
-		wikiProject.idString.separator ??= '.';
-		wikiProject.idString.direction ??= 'desc';
+		wikiProject.idString.separator ??= wikiProjectSchema.idString.properties.separator.default;
+		wikiProject.idString.direction ??= wikiProjectSchema.idString.properties.direction.default;
 	}
-	wikiProject.regexPaths ??= false;
-	wikiProject.wikiFarm ??= null;
-	wikiProject.extensions ??= [];
-	wikiProject.urlSpaceReplacement ??= '_';
-	wikiProject.note ??= null;
+	wikiProject.regexPaths ??= wikiProjectSchema.regexPaths.default;
+	wikiProject.wikiFarm ??= wikiProjectSchema.wikiFarm.default;
+	wikiProject.extensions ??= wikiProjectSchema.extensions.default.slice();
+	wikiProject.urlSpaceReplacement ??= wikiProjectSchema.urlSpaceReplacement.default;
+	wikiProject.note ??= wikiProjectSchema.note.default;
 	return wikiProject;
 } );
 
 /**
  * 
  * @param {string} input 
- * @returns {?[URL, WikiProject]}
+ * @returns {?{fullArticlePath: string, fullScriptPath: string, wikiProject: WikiProject}}
  */
 function inputToWikiProject(input) {
-	let wikiProject = wikiProjects.find( wikiProject => input.split('/')[2].endsWith( wikiProject.name ) );
+	if ( functionCache.inputToWikiProject.has(input) ) return structuredClone(functionCache.inputToWikiProject.get(input));
+	let result = null;
+	let wikiProject = wikiProjects.find( wikiProject => input.split('/').slice(0, 3).some( part => part.endsWith( wikiProject.name ) ) );
 	if ( wikiProject ) {
 		let articlePath = ( wikiProject.regexPaths ? '/' : wikiProject.articlePath );
 		let scriptPath = ( wikiProject.regexPaths ? '/' : wikiProject.scriptPath );
 		let regex = input.match( new RegExp( wikiProject.regex + `(?:${articlePath}|${scriptPath}|/?$)` ) );
 		if ( regex ) {
-			if ( wikiProject.regexPaths ) scriptPath = wikiProject.scriptPath.replace( /\$(\d)/g, (match, n) => regex[n] );
-			return [new URL('https://' + regex[1] + scriptPath), wikiProject];
+			if ( wikiProject.regexPaths ) {
+				scriptPath = wikiProject.scriptPath.replace( /\$(\d)/g, (match, n) => regex[n] );
+				articlePath = wikiProject.articlePath.replace( /\$(\d)/g, (match, n) => regex[n] );
+			}
+			result = {
+				fullArticlePath: 'https://' + regex[1] + articlePath,
+				fullScriptPath: 'https://' + regex[1] + scriptPath,
+				wikiProject: wikiProject
+			};
 		}
 	}
-	return null;
+	functionCache.inputToWikiProject.set(input, result);
+	return structuredClone(result);
 }
 
 /**
@@ -61,15 +84,18 @@ function inputToWikiProject(input) {
  * @returns {?string}
  */
 function urlToIdString(url) {
+	if ( functionCache.urlToIdString.has(url.href) ) return functionCache.urlToIdString.get(url.href);
+	let result = null;
 	let wikiProject = wikiProjects.find( wikiProject => wikiProject.idString && url.hostname.endsWith( wikiProject.name ) );
 	if ( wikiProject ) {
 		let regex = url.href.match( new RegExp( wikiProject.regex ) )?.slice(2);
 		if ( regex?.length ) {
 			if ( wikiProject.idString.direction === 'desc' ) regex.reverse();
-			return regex.join(wikiProject.idString.separator);
+			result = regex.join(wikiProject.idString.separator);
 		}
 	}
-	return null;
+	functionCache.urlToIdString.set(url.href, result);
+	return result;
 }
 
 /**
@@ -79,14 +105,21 @@ function urlToIdString(url) {
  * @returns {?URL}
  */
 function idStringToUrl(idString, projectName) {
+	let cacheKey = JSON.stringify([idString,projectName]);
+	if ( functionCache.idStringToUrl.has(cacheKey) ) {
+		let result = functionCache.idStringToUrl.get(cacheKey);
+		return ( result ? new URL(result) : result );
+	}
+	let result = null;
 	let wikiProject = wikiProjects.find( wikiProject => wikiProject.idString && wikiProject.name === projectName )?.idString;
 	if ( wikiProject ) {
 		let regex = idString.match( new RegExp( '^' + wikiProject.regex + '$' ) )?.[1].split(wikiProject.separator);
 		if ( regex && regex.length <= wikiProject.scriptPaths.length ) {
-			return new URL(wikiProject.scriptPaths[regex.length - 1].replace( /\$(\d)/g, (match, n) => regex[n - 1] ));
+			result = wikiProject.scriptPaths[regex.length - 1].replace( /\$(\d)/g, (match, n) => regex[n - 1] );
 		}
 	}
-	return null;
+	functionCache.idStringToUrl.set(cacheKey, result);
+	return ( result ? new URL(result) : result );
 }
 
 module.exports = {
